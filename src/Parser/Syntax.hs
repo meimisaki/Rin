21ee886b920e42c7094a5af8a49cd276e77df4eb
meimisaki <meimisaki@gmail.com>
2016-04-16 04:14:04 +0800

    simplify syntax definition

--- src/Parser/Syntax.hs
+++ src/Parser/Syntax.hs
@@ -5,6 +5,8 @@ module Parser.Syntax
 
 import Common
 
+import Data.Char
+import Type.PrettyPrint
 import Type.Types
 
 data Dec
@@ -12,80 +14,99 @@ data Dec
   | ValD Pat Body [Dec]
   | DataD Name [Name] [Con]
   | SigD [Name] Type
-  | InfixD Fixity Name
+  | InfixD Fixity [Name]
+  deriving Show
 
 type Clause = ([Pat], Body, [Dec])
 
 data Body
   = NormalB Exp
   | GuardedB [(Guard, Exp)]
+  deriving Show
 
-data Guard
-  = NormalG Exp
-  | PatG [Stmt]
-
-data Stmt
-  = BindS Pat Exp
-  | LetS [Dec]
-  | NoBindS Exp
+type Guard = Exp
 
 data Exp
   = VarE Name
   | ConE Name
   | LitE Lit
   | AppE Exp Exp
-  | InfixE (Maybe Exp) Name (Maybe Exp)
+  | LSecE Exp Name
+  | RSecE Name Exp
+  | InfixE Exp Name Exp
   | UInfixE Exp Name Exp
   | ParensE Exp -- necessary, since we leave infix expression unresolved
   | LamE [Pat] Exp
-  | TupE [Maybe Exp]
+  | TupE [Exp]
   | ListE [Exp]
   | CondE Exp Exp Exp
-  | LetE [Dec] Exp
   | CaseE Exp [Match]
-  | DoE [Stmt]
+  | LetE [Dec] Exp
   | SigE Exp Type
+  -- for patterns
+  | AsE Name Pat
+  | WildE
 -- TODO: arith sequences, list comprehensions, etc.
+  deriving Show
+
+type Pat = Exp
 
 type Match = (Pat, Body, [Dec])
 
 data Lit
   = CharL Char
-  | IntL Int
-  | FloatL Double
   | StringL String
-
-tupCon :: Int -> Type
-tupCon len = TyCon (replicate len ',')
-
-listCon :: Type
-listCon = TyCon "[]"
+  | NumberL Int
+  deriving Show
 
 data Con
   = NormalC Name [Type]
   | InfixC Type Name Type
-
-data Pat
-  = VarP Name
-  | ConP Name [Pat]
-  | LitP Lit
-  | InfixP Pat Name Pat
-  | UInfixP Pat Name Pat
-  | ParensP Pat
-  | AsP Name Pat
-  | TupP [Pat]
-  | ListP [Pat]
-  | WildP
+  deriving Show
 
 data Fixity = Fixity Assoc Int
+  deriving Show
 
 data Assoc = InfixN | InfixL | InfixR
+  deriving Show
 
-minPrecedence :: Int
-minPrecedence = 0
+isVarId :: Name -> Bool
+isVarId x = case x of
+  ch:_ | isLower ch || ch == '_' -> True
+  _ -> False
 
-maxPrecedence :: Int
-maxPrecedence = 9
+isConId :: Name -> Bool
+isConId x = case x of
+  ch:_ | isUpper ch -> True
+  _ -> False
+
+isVarSym :: Name -> Bool
+isVarSym x = case x of
+  ch:_ | ch /= ':' -> True
+  _ -> False
+
+isConSym :: Name -> Bool
+isConSym x = case x of
+  ':':_ -> True
+  _ -> False
+
+minPrec :: Int
+minPrec = 0
+
+maxPrec :: Int
+maxPrec = 9
+
+defaultAssoc :: Assoc
+defaultAssoc = InfixL
+
+defaultPrec :: Int
+defaultPrec = maxPrec
 
 defaultFixity :: Fixity
-defaultFixity = Fixity InfixL maxPrecedence
+defaultFixity = Fixity defaultAssoc defaultPrec
+
+tupCon :: Int -> Name
+tupCon n = "(" ++ replicate n ',' ++ ")"
+
+listCon :: Name
+listCon = "[]"
